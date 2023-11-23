f1b9d7f15da78c64ca6e27e9e38b2de57ac53b7f
i-moonlight <infinite.moonlight0506@gmail.com>
2023-11-24 00:35:05 +0200

    add TokenService

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using DatingApp.Data;
+using DatingApp.Services.TokenService;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService, TokenService>();
+
 
 var app = builder.Build();
 
