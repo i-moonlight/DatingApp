9e47eb236380ca6fe8314e30e6c603f65919ad9a
i-moonlight <infinite.moonlight0506@gmail.com>
2023-11-23 22:48:54 +0200

    update user entity + migration database

--- Data/Migrations/20231123204510_UserPasswordAdded.Designer.cs
+++ Data/Migrations/20231123204510_UserPasswordAdded.Designer.cs
@@ -2,6 +2,7 @@
 using DatingApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingApp.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231123204510_UserPasswordAdded")]
+    partial class UserPasswordAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
 
     }
 }
