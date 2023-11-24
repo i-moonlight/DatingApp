af938e924f5951c41802718811b2c65da53cf8ae
i-moonlight <infinite.moonlight0506@gmail.com>
2023-11-25 00:51:36 +0200

    add dropdown

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../services/account.service';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgbDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
   providers:[AccountService]
@@ -26,6 +27,10 @@ export class NavComponent implements OnInit{
       },
       error: error => console.log(error)
     });
+  }
+  logout(){
+    this.loggenIn =  false;
+    
   }
 
 }
