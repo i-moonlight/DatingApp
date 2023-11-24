15d1fedbfc5f45ccd559d0a8ca621650c07e761c
i-moonlight <infinite.moonlight0506@gmail.com>
2023-11-24 14:19:24 +0200

    add Auntithication, add Authorization

---
+++ Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using DatingApp.Data;
+using DatingApp.Services.TokenService;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,26 @@
 using DatingApp.Data;
+using DatingApp.Extensions;
 using DatingApp.Services.TokenService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenService>();
-
-
+builder.Services.AddApplicationService(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .WithOrigins("http://localHost:4200"));
 
-
-//app.UseHttpsRedirection();
-
-//app.UseAuthorization();
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localHost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
