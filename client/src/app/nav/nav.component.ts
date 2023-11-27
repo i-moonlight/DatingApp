84864b24301636deae3716bbb1813deddeba31b0
i-moonlight <infinite.moonlight0506@gmail.com>
2023-11-27 19:34:37 +0200

    add async pipe for login

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../services/account.service';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { User } from '../model/User';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -14,33 +15,35 @@ import { User } from '../model/User';
 })
 export class NavComponent implements OnInit{
   model: any={}
-  loggenIn = false;
+  //currentUser$: Observable<User | null> = of(null);
+  //loggenIn = false;
   ngOnInit(): void {
-    this.getCurrentUser();
+    //this.currentUser$ = this.accoutnService.currentUser$;
+    //this.getCurrentUser();
    }
 
-  constructor(private accoutnService:AccountService){ }
+  constructor(public accoutnService:AccountService){ }
 
-  getCurrentUser(){
+  // getCurrentUser(){
    
-    this.accoutnService.currentUser$.subscribe({
-      next: user => this.loggenIn = !!user,
-      error: error => console.log(error)
-    })
-  }
+  //   this.accoutnService.currentUser$.subscribe({
+  //     next: user => this.loggenIn = !!user,
+  //     error: error => console.log(error)
+  //   })
+  // }
 
   login(){
     this.accoutnService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggenIn = true;
+        //this.loggenIn = true;
       },
       error: error => console.log(error)
     });
   }
   logout(){
     this.accoutnService.logout();
-    this.loggenIn =  false;
+    //this.loggenIn =  false;
     
   }
 
