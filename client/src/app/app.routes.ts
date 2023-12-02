869dbc52a278c6a735325844ead083f08d9ba177
i-moonlight <infinite.moonlight0506@gmail.com>
2023-12-03 00:52:56 +0200

    add routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListComponent } from './list/list.component';
+import { MessagesComponent } from './messages/messages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'members', component: MemberListComponent},
+    {path: 'member/:id', component: MemberDetailComponent},
+    {path: 'lists', component: ListComponent},
+    {path: 'messages', component:MessagesComponent},
+    {path: '**', component:HomeComponent, pathMatch: 'full'}
+];
