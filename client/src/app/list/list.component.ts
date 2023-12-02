5130b6d6820e6c86b6bdc9367dcd16cb4e86dc53
i-moonlight <infinite.moonlight0506@gmail.com>
2023-12-02 23:40:32 +0200

    add new components

---
+++ client/src/app/list/list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+
+}
---
+++ client/src/app/members/member-list/member-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-member-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './member-list.component.html',
+  styleUrl: './member-list.component.css'
+})
+export class MemberListComponent {
+
+}
