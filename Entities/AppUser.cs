501d01cb4a8853147ca32fdaea101dac3ae2de5c
i-moonlight <infinite.moonlight0506@gmail.com>
2023-12-08 23:16:57 +0200

    update User create Photo entity

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -9,6 +9,19 @@ namespace DatingApp.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string KnowAs {  get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+        public DateTime LastActive {  get; set; }
+        public string Gender {  get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public List<Photo> Photos { get; set; } = new();
+
 
     }
 }
---
+++ Entities/Photo.cs
@@ -0,0 +1,10 @@
+﻿namespace DatingApp.Entities
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+        public string PublicId { get; set; }
+    }
+}
