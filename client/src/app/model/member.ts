4b27826df7c1c4ab24adc0217d7b85c1a4e74bc6
i-moonlight <infinite.moonlight0506@gmail.com>
2023-12-13 23:15:12 +0200

    add member

---
+++ client/src/app/model/member.ts
@@ -0,0 +1,19 @@
+import { Photo } from "./Photo"
+
+export interface Member {
+    id: number
+    userName: string
+    photoUrl: string
+    age: number
+    knowAs: string
+    created: Date
+    lastActive: Date
+    gender: string
+    introduction: string
+    lookingFor: string
+    interests: string
+    city: string
+    country: string
+    photos: Photo[]
+  }
+  
