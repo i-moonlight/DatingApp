4d1fd252d613f4556af41a59e9a06245cbdafc29
i-moonlight <infinite.moonlight0506@gmail.com>
2023-12-19 11:18:36 +0200

    update member-detail (add gallery)

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Member } from '../../model/member';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-member-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './member-card.component.html',
   styleUrl: './member-card.component.css'
 })
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { Member } from '../../model/member';
+import { MembersService } from '../../services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { GALLERY_CONFIG, GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 @Component({
   selector: 'app-member-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbNavModule, GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit {
+  member: Member | undefined;
+  images: GalleryItem[] = [];
+  constructor(private memberService: MembersService,
+    private route: ActivatedRoute) {
 
+  }
+  ngOnInit(): void {
+    this.loadMember();
+  }
+  loadMember() {
+    var username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: member => {
+        this.member = member
+        this.getImages();
+      }
+    });
+  }
+
+  getImages(){
+    if(!this.member) return;
+
+    for (const photo of this.member.photos){    
+      this.images.push(new ImageItem({src: photo.url, thumb: photo.url}));
+    }
+  }
 }
